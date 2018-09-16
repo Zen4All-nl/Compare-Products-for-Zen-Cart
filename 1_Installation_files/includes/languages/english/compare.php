af74f6a7b702d0dd80697bb3f0a4759238c037f1
Zen4All <info@zen4all.nl>
2018-09-16 13:29:29 +0200

    mkae use of database value for max products

--- 1_Installation_files/includes/languages/english/compare.php
+++ 1_Installation_files/includes/languages/english/compare.php
@@ -17,7 +17,7 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: compare.php 2011-01-28 5:23:52MT brit (docreativedesign.com) $
+// $Id: compare.php 2018-09-16 5:23:52MT Zen4All (https://zen4all.nl) $
 //
 
 define('NAVBAR_TITLE', 'Product Comparison');
@@ -31,9 +31,8 @@ define('COMPARE_MANUFACTURER', 'Manufacturer: ');
 define('COMPARE_DESC', 'Description: ');
 define('COMPARE_DEFAULT', 'Compare');
 define('COMPARE_REMOVE', 'remove');
-define('COMPARE_COUNT', 'up to 4 items');
+define('COMPARE_COUNT', 'up to ' . COMPARE_VALUE_COUNT . ' items');
 define('COMPARE_WARNING_START', 'only ');
 define('NOTHING_TO_COMPARE', '<div class="compNothing">No products to compare</div>');
 define('COMPARE_WARNING_END', ' items can be compared at one time');
 define('TEXT_INFORMATION', 'This page is to show product comparisons for selected items in the session.');
-?>
