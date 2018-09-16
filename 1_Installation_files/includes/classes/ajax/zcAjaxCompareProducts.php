558f7c3bd1ea4857698dacb47c11a93d52471627
Zen4All <info@zen4all.nl>
2018-09-16 16:03:12 +0200

    make changes for less core changes

--- 1_Installation_files/includes/classes/ajax/zcAjaxCompareProducts.php
+++ 1_Installation_files/includes/classes/ajax/zcAjaxCompareProducts.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * ajax_compare.php
+ * zcAjaxCompareProducts.php
  * ajax call to show products selected for comparison
  *
  * @package general
@@ -10,66 +10,84 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: ajax_compare.php 00001 2011-01-28 5:23:52MT brit (docreativedesign.com) $
  */
-class zcAjaxCompare extends base {
+class zcAjaxCompareProducts extends base {
 
-  public function updateCompare() {
-
-    global $db;
-// get values
-    $action = $_POST['action'];
-    $selected = $_POST['compare_id'];
-    $compare_array = array();
-    $comp_images = '';
-    $compare_warning = '';
-
-    $comp_value_count = count($_SESSION['compare']);
+  var $selected;
+  var $compare_array;
+  var $comp_images;
+  var $compare_warning;
+  var $comp_value_count;
 
 // add new products selected
-    if ($action == 'add') {
-      if ($comp_value_count < COMPARE_VALUE_COUNT) {
-        $compare_array[] = $selected;
-        if (isset($_SESSION['compare'])) {
-          foreach ($_SESSION['compare'] as $c) {
-            if ($c != $selected) {
-              $compare_array[] = $c;
-            }
-          }
+  public function addProduct() {
+    $returndata['toApi'] = $_POST;
+    $selected = $_POST['compare_id'];
+    $compare_array = array();
+    $compare_warning = '';
+    $comp_value_count = count($_SESSION['compare']);
+
+    include(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_DEFINE_COMPARE_LANGUAGE);
+
+    if ($comp_value_count < COMPARE_VALUE_COUNT) {
+      $compare_array[] = $selected;
+      foreach ($_SESSION['compare'] as $c) {
+        if ($c != $selected) {
+          $compare_array[] = $c;
         }
-        $_SESSION['compare'] = array_unique($compare_array);
-      } else {
-        $compare_warning = '<div id="compareWarning">' . COMPARE_WARNING_START . COMPARE_VALUE_COUNT . COMPARE_WARNING_END . '</div>';
       }
+      $_SESSION['compare'] = array_unique($compare_array);
+    } else {
+      $compare_warning = '<div id="compareWarning">' . COMPARE_WARNING_START . COMPARE_VALUE_COUNT . COMPARE_WARNING_END . '</div>';
     }
+    $result = $this->getProducts($_SESSION['compare']);
+    return([
+      'data' => $result,
+      'toApi' => $returndata['toApi']
+    ]);
+  }
 
 // remove products
-    if ($action == 'remove') {
-      foreach ($_SESSION['compare'] as $rValue) {
-        if ($rValue != $selected) {
-          $removed_compare_array[] = $rValue;
-        }
-        $_SESSION['compare'] = array_unique($removed_compare_array);
+  public function removeProduct() {
+    $returndata['toApi'] = $_POST;
+    $selected = $_POST['compare_id'];
+    foreach ($_SESSION['compare'] as $rValue) {
+      if ($rValue != $selected) {
+        $removed_compare_array[] = $rValue;
       }
+      $_SESSION['compare'] = array_unique($removed_compare_array);
     }
 
+    $result = $this->getProducts($_SESSION['compare']);
+    return([
+      'data' => $result,
+      'toApi' => $returndata['toApi']
+    ]);
+  }
+
+  private function getProducts($compareList) {
+    global $db;
+    $comp_images = '';
 // return new value for the session
-    foreach ($_SESSION['compare'] as $value) {
+    foreach ($compareList as $value) {
       if (!empty($value)) {
         $product_comp_image = $db->Execute("SELECT p.products_id, p.master_categories_id, pd.products_name, p.products_image
                                         FROM " . TABLE_PRODUCTS . " p
                                         LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd ON pd.products_id = p.products_id
                                         WHERE p.products_id = " . (int)$value);
-    $comp_images .= '<div class="compareAdded"><a href="' . zen_href_link(zen_get_info_page($product_comp_image->fields['products_id']), 'cPath=' . (zen_get_generated_category_path_rev($product_comp_image->fields['master_categories_id'])) . '&products_id=' . $product_comp_image->fields['products_id']) . '">' . zen_image(DIR_WS_IMAGES . $product_comp_image->fields['products_image'], $product_comp_image->fields['products_name'], '', '35', 'class="listingProductImage"') . '</a><div>' . '<a onclick="compareNew(' . $product_comp_image->fields['products_id'] . ', \'remove\')" title="remove">' . COMPARE_REMOVE . '</a>' . '</div></div>';
-  }
-}
+        $comp_images .= '<div class="compareAdded">';
+        $comp_images .= '<a href="' . zen_href_link(zen_get_info_page($product_comp_image->fields['products_id']), 'cPath=' . (zen_get_generated_category_path_rev($product_comp_image->fields['master_categories_id'])) . '&products_id=' . $product_comp_image->fields['products_id']) . '">' . zen_image(DIR_WS_IMAGES . $product_comp_image->fields['products_image'], $product_comp_image->fields['products_name'], '', '35', 'class="listingProductImage"') . '</a>';
+        $comp_images .= '<div>';
+        $comp_images .= '<button type="button" onclick="compare(\'' . $product_comp_image->fields['products_id'] . '\', \'removeProduct\')" title="remove" class="btn btn-default btn-xs">' . COMPARE_REMOVE . '</button>';
+        $comp_images .= '</div>';
+        $comp_images .= '</div>';
+      }
+    }
 
 // return HTML view of found products
-if (!empty($comp_images)) {
-  echo '<div id="compareMainWrapper"><div class="compareAdded compareButton">' . '<a href="' . zen_href_link('compare') . '" title="compare">' . '<span class="cssButton">' . COMPARE_DEFAULT . '</span></a></div>' . $comp_images . '</div>';
-}
-echo '<br class="clearBoth" />';
-
-// send back warning if more than allowed is selected
-    echo $compare_warning;
+    if (!empty($comp_images)) {
+      $data = '<div id="compareMainWrapper"><div class="compareAdded compareButton">' . '<a href="' . zen_href_link('compare') . '" title="compare">' . '<span class="cssButton">' . COMPARE_DEFAULT . '</span></a></div>' . $comp_images . '</div>';
+    }
+    return $data;
   }
 
 }
--- 1_Installation_files/includes/templates/YOUR_TEMPLATE/jscript/jscript_compareProducts.php
+++ 1_Installation_files/includes/templates/YOUR_TEMPLATE/jscript/jscript_compareProducts.php
@@ -1,11 +1,45 @@
 <script>
-  function compareNew(productId, action) {
-    zcJS.ajax({
-      url: 'ajax.php?act=ajaxCompareProducts&method=' + action,
-      data: {compare_id: productId}
-    }).done(function (returnData) {
-      console.log(returnData);
-      $('#compareProducts').html(returnData['data']);
-    });
+  var compareResultDiv;
+
+  function compare(productId, action) {
+      zcJS.ajax({
+          url: 'ajax.php?act=ajaxCompareProducts&method=' + action,
+          data: {compare_id: productId}
+      }).done(function (returnData) {
+          console.log(returnData);
+          $('#compareProducts').html(returnData['data']);
+      });
   }
+  compareResultDiv = '';
+  compareResultDiv += '<div id="compareResult" class="group">' + "\n";
+  compareResultDiv += '  <div class="back compareText"><strong><?php echo COMPARE_DEFAULT; ?></strong> <?php echo COMPARE_COUNT_START . COMPARE_VALUE_COUNT . COMPARE_COUNT_END; ?></div>' + "\n";
+  compareResultDiv += '    <div id="compareProducts">' + "\n";
+<?php if (!empty($_SESSION['compare'])) { ?>
+  compareResultDiv += '      <div id="compareMainWrapper">' + "\n";
+  compareResultDiv += '        <div class="compareAdded compareButton">' + "\n";
+  compareResultDiv += '          <a href="index.php?main_page=compare" title="compare"><span class="cssButton" class="btn btn-primary" role="button"><?php echo COMPARE_DEFAULT; ?></span></a>' + "\n";
+  compareResultDiv += '        </div>' + "\n";
+  <?php
+  foreach ($_SESSION['compare'] as $value) {
+    $product_comp_image = $db->Execute("SELECT p.products_id, p.master_categories_id, pd.products_name, p.products_image
+                                        FROM " . TABLE_PRODUCTS . " p
+                                        LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd ON pd.products_id = p.products_id
+                                        WHERE p.products_id = " . (int)$value);
+    ?>
+  compareResultDiv += '        <div class="compareAdded">' + "\n";
+  compareResultDiv += '          <a href="<?php echo zen_href_link(zen_get_info_page($product_comp_image->fields['products_id']), 'cPath=' . (zen_get_generated_category_path_rev($product_comp_image->fields['master_categories_id'])) . '&products_id=' . $product_comp_image->fields['products_id']); ?>"><?php echo zen_image(DIR_WS_IMAGES . $product_comp_image->fields['products_image'], $product_comp_image->fields['products_name'], '', '35', 'class="listingProductImage"'); ?></a>' + "\n";
+  compareResultDiv += '          <div>' + "\n";
+  compareResultDiv += '            <button type="button" onclick="compare(\'<?php echo $product_comp_image->fields['products_id']; ?>\', \'removeProduct\')" title="remove" class="btn btn-default btn-xs"><?php echo COMPARE_REMOVE; ?></button>' + "\n";
+  compareResultDiv += '          </div>' + "\n";
+  compareResultDiv += '        </div>' + "\n";
+  <?php } ?>
+  compareResultDiv += '      </div>' + "\n";
+<?php } ?>
+  compareResultDiv += '    </div>' + "\n";
+  compareResultDiv += '  </div>' + "\n";
+  compareResultDiv += '</div>' + "\n";
+
+$(document).ready(function(){
+  $('#filter-wrapper').after(compareResultDiv);
+  });
 </script>
