b5e5615ae2eb809f871c6c4b4cdae75b9fc9ac23
Zen4All <info@zen4all.nl>
2018-09-17 21:24:35 +0200

    add defines previously living in english,php

---
+++ 1_Installation_files/includes/languages/english/extra_definitions/compare.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: index.php 6550 2007-07-05 03:54:54Z drbyte $
+ * @modification: added in the language defines for the comparison system 2011-01-28 5:23:52MT brit (docreativedesign.com) $
+ */
+
+  define('COMPARE_DEFAULT', 'Compare');
+  define('COMPARE_REMOVE', 'remove');
+  define('COMPARE_COUNT_START', 'up to ');
+  define('COMPARE_COUNT_END', ' items');
+  
+  define('TABLE_HEADING_COMPARE', 'Compare');
