cf16b6b7292b85c7cce7c5ec0a12aab796868c7d
Zen4All <info@zen4all.nl>
2020-01-05 21:23:45 +0100

    Add observers for New, All, and Featured lisitng

--- 1_Installation_files/includes/classes/observers/CompareProductsObserver.php
+++ 1_Installation_files/includes/classes/observers/CompareProductsObserver.php
@@ -10,12 +10,23 @@ class CompareProductsObserver extends base {
 
   public function __construct()
   {
-    if ((defined('COMPARE_PRODUCTS_STATUS') && COMPARE_PRODUCTS_STATUS == 'true') && (defined('COMPARE_PRODUCTS_LIST_STATUS') && COMPARE_PRODUCTS_LIST_STATUS == 'true')) {
-      $this->attach($this, array('NOTIFY_PRODUCT_LISTING_END'));
+    if (defined('COMPARE_PRODUCTS_STATUS') && COMPARE_PRODUCTS_STATUS == 'true') {
+      if (defined('COMPARE_PRODUCTS_LIST_STATUS') && COMPARE_PRODUCTS_LIST_STATUS == 'true') {
+        $this->attach($this, array('NOTIFY_PRODUCT_LISTING_END'));
+      }
+      if (defined('COMPARE_PRODUCTS_FEATURED_STATUS') && COMPARE_PRODUCTS_FEATURED_STATUS == 'true') {
+        $this->attach($this, array('NOTIFY_PRODUCTS_FEATURED_END'));
+      }
+      if (defined('COMPARE_PRODUCTS_NEW_STATUS') && COMPARE_PRODUCTS_NEW_STATUS == 'true') {
+        $this->attach($this, array('NOTIFY_PRODUCTS_NEW_END'));
+      }
+      if (defined('COMPARE_PRODUCTS_ALL_STATUS') && COMPARE_PRODUCTS_ALL_STATUS == 'true') {
+        $this->attach($this, array('NOTIFY_PRODUCTS_ALL_END'));
+      }
     }
   }
 
-  public function update(&$class, $eventID, $p1, &$list_box_contents, &$p3, &$p4, &$p5, &$p6, &$p7)
+  public function update(&$class, $eventID, $p1, &$p2, &$p3, &$p4, &$p5, &$p6, &$p7)
   {
     switch ($eventID) {
       case 'NOTIFY_PRODUCT_LISTING_END':
@@ -25,12 +36,19 @@ class CompareProductsObserver extends base {
           $rows++;
           $lc_align = '';
           $lc_text = '<div id="compareSelectProductId_' . $item['products_id'] . '" class="compareSelect list-compare"><button type="button" id="buttonCompareSelectProductId_' . $item['products_id'] . '" onclick="compare(\'' . $item['products_id'] . '\',\'addProduct\');"><i class="fa fa-plus"></i> ' . COMPARE_DEFAULT . '</button></div>';
-          $list_box_contents[$rows][] = array('align' => $lc_align,
+          $p2[$rows][] = array('align' => $lc_align,
             'params' => 'class="productListing-data"',
             'text' => $lc_text);
         }
         break;
+      case 'NOTIFY_PRODUCTS_FEATURED_END':
+      case 'NOTIFY_PRODUCTS_NEW_END':
+      case 'NOTIFY_PRODUCTS_ALL_END':
 
+        echo '<div id="compareSelectProductId_' . $p1->fields['products_id'] . '" class="compareSelect list-compare">';
+        echo '<button type="button" id="buttonCompareSelectProductId_' . $p1->fields['products_id'] . '" onclick="compare(\'' . $p1->fields['products_id'] . '\', \'addProduct\');"><i class="fa fa-plus"></i> ' . COMPARE_DEFAULT . '</button>';
+        echo '</div>';
+        break;
       default:
         break;
     }
