e9e083ebc8cc8d7524c2f6f05f028b8d5298ae54
Zen4All <info@zen4all.nl>
2020-01-07 15:03:12 +0100

    remove duplicate if statement

--- 1_Installation_files/includes/modules/pages/compare/main_template_vars.php
+++ 1_Installation_files/includes/modules/pages/compare/main_template_vars.php
@@ -12,7 +12,8 @@
 if (isset($_SESSION['compareProducts']) && $_SESSION['compareProducts'] != '') {
   $compare_info = array();
   $result = array();
-  $removeProduct = $_GET['removeProduct'];
+  $compareResult = array();
+  $removeProduct = (int)$_GET['removeProduct'];
 
   if ($removeProduct > 0) {
     $removedCompareArray = array();
@@ -25,25 +26,23 @@ if (isset($_SESSION['compareProducts']) && $_SESSION['compareProducts'] != '') {
   }
 
   // loop session for products
-  if (isset($_SESSION['compareProducts']) && $_SESSION['compareProducts'] != '') {
-    foreach ($_SESSION['compareProducts'] as $value) {
-      $productsQuery = "SELECT p.products_id, p.products_quantity, p.products_model, p.products_image, p.products_weight, p.master_categories_id,
-                               pd.products_name, pd.products_description,
-                               m.manufacturers_name
-                        FROM " . TABLE_PRODUCTS . " p
-                        LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd ON p.products_id = pd.products_id
-                          AND pd.language_id = " . (int)$_SESSION['languages_id'] . "
-                        LEFT JOIN " . TABLE_MANUFACTURERS . " m ON p.manufacturers_id = m.manufacturers_id
-                        WHERE p.products_status = 1
-                        AND p.products_id = " . (int)$value;
+  foreach ($_SESSION['compareProducts'] as $value) {
+    $productsQuery = "SELECT p.products_id, p.products_quantity, p.products_model, p.products_image, p.products_weight, p.master_categories_id,
+                             pd.products_name, pd.products_description,
+                             m.manufacturers_name
+                      FROM " . TABLE_PRODUCTS . " p
+                      LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd ON p.products_id = pd.products_id
+                        AND pd.language_id = " . (int)$_SESSION['languages_id'] . "
+                      LEFT JOIN " . TABLE_MANUFACTURERS . " m ON p.manufacturers_id = m.manufacturers_id
+                      WHERE p.products_status = 1
+                      AND p.products_id = " . (int)$value;
 
-      $products = $db->Execute($productsQuery);
+    $products = $db->Execute($productsQuery);
 
       foreach ($products->fields as $key => $item) {
         $compareResult[$value][$key] = $item;
       }
     }
   }
-}
 
 require($template->get_template_dir('tpl_compare_default.php', DIR_WS_TEMPLATE, $current_page_base, 'templates') . '/tpl_compare_default.php');
